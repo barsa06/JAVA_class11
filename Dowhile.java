6945885d5456b05ce80518079387e694bf500866
unknown <bpun7891@gmail.com>
2025-11-19 11:48:35 +0545

    added loop statements

---
+++ Dowhile.java
@@ -0,0 +1,10 @@
+public class Dowhile{
+    public static void main( String args[]){
+        System.out.println("printing first five natural numbers");
+        do{
+            int i=1;
+            System.out.println(i);
+            i++ ; 
+        }while(i<=5);
+ }
+}
---
+++ Forloop.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Forloop{
+    public static void main(String args[]){
+       Scanner input = new Scanner(System.in);
+       System.out.println("enter number upto which you want to print");
+       for(int i=1; i<=5; i++){
+        System.out.println(i);
+       }
+    }
+}
