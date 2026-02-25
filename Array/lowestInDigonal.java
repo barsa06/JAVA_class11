600623fef3338293beeecd286445b265e3903c58
unknown <bpun7891@gmail.com>
2026-02-25 11:39:49 +0545

    file handling demo

---
+++ Array/lowestInDigonal.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class SumOfDigonal{
+    public static void main(String args[]){
+        Scanner input= new Scanner(System.in);
+
+        System.out.println("Enter the no of rows:");
+        int rows=input.nextInt();
+
+        System.out.println("enter the no of columns:");
+        int cols=input.nextInt();
+        int sum=0;
+
+        int [][] a=new int [rows][cols];
+        for(int i=0; i<rows; i++){
+           for(int j=0; j<cols; j++){
+               System.out.print("a["+i+"]["+j+"]:");
+               a[i][j]=input.nextInt();
+            }
+            
+        }
+    }
+}
